9ce6ff1a0fd3eeb53c0187a3911de3df20c092b3
Caleb Stepanian <ziphilt@gmail.com>
2014-12-06 04:26:47 -0500

    Add GTK dependencies and demo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # phim
 
 The Vim of Photo Managers
+
+## Building
+
+    cabal sandbox init
+    cabal install gtk2hs-buildtools
+    cabal install
--- phim.hs
+++ phim.hs
@@ -1,2 +1,14 @@
+import qualified Graphics.UI.Gtk as G
+import Graphics.UI.Gtk (AttrOp((:=)))
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+        G.initGUI
+        window <- G.windowNew
+        button <- G.buttonNew
+        G.set window [G.containerBorderWidth := 10, G.containerChild := button]
+        G.set button [G.buttonLabel := "Hello World"]
+        G.onClicked button (putStrLn "Hello World")
+        G.onDestroy window G.mainQuit
+        G.widgetShowAll window
+        G.mainGUI
